8b605059278e12f5f771af855563acdd307c2bca
Peter Amrehn <peter.wendorff@gmx.de>
2018-05-22 20:47:12 +0200

    initial commit -

---
+++ SharpImageSource/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SharpImageSource
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ SharpImageSource/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Windows.Media;
+using ImageSharp.WpfImageSource;
+using ImageSharp;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Filters;
+
+namespace SharpImageSource
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var stream = assembly.GetManifestResourceStream("SharpImageSource.AutoTranslateWindowsStore.png");
+            var image = Image.Load<Rgba32>(stream);
+            this.ImageSharpSource = new ImageSharpImageSource<SixLabors.ImageSharp.PixelFormats.Rgba32>()
+            {
+                Source = image
+            };
+
+            InitializeComponent();
+        }
+
+        public ImageSource ImageSharpSource { get; }
+    }
+}
